aff464593f6b3824ecaea31749bc2da4dbf9e797
Mirosław Waldowski <mwa@amu.edu.pl>
2020-10-01 01:19:17 +0200

    make it better

--- db/db.js
+++ db/db.js
@@ -10,16 +10,6 @@ const pool = new Pool({
   ssl: isProduction,
 });
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_DATABASE,
-//   //max: 25, // set pool max size to 20
-//   //idleTimeoutMillis: 5000, // close idle clients after 5 second
-// });
-
 module.exports = pool;
 
 
